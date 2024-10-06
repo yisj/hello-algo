29cb7721389383b4bd0d9bbc34af3e86bb04921b
Seungjae Lee <fitdark@naver.com>
2024-10-06 13:32:41 +0900

    [level 3] Title: 정수 삼각형, Time: 9.68 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -8,27 +8,17 @@ public class Solution {
             dp[i] = new int[i+1];
         }
 
-        dp[0][0] = triangle[0][0];
-        for(int i=1; i<triangle.length; i++){
-            dp[i][0] = dp[i-1][0] + triangle[i][0];
-            for(int j=1; j<i; j++){
-                dp[i][j] = Math.max(dp[i-1][j-1]+triangle[i][j],dp[i-1][j]+triangle[i][j]);
-            }
-            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
-        }
+        for(int j=0; j<triangle.length; j++)
+            dp[triangle.length-1][j] = triangle[triangle.length-1][j];
 
-        // for(int i=0; i<triangle.length; i++){
-        //     System.out.println(Arrays.toString(dp[i]));
-        // }
+
+        for(int i=triangle.length-2; i>=0; i--){
+            for(int j=0; j<=i;j++){
+                dp[i][j] = triangle[i][j]+Math.max(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
 
         
-
-        int answer = 0;
-        for(int j=0; j<triangle.length; j++){
-            if(dp[triangle.length-1][j] > answer)
-                answer = dp[triangle.length-1][j];
-        }
-        return answer;
+        return dp[0][0];
     }
-
 }
